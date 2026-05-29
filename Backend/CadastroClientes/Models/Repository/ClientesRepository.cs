77b39210073b495b28257b49f08980e1c6a53a2c
amandacrystina <acrystina947@gmail.com>
2026-05-29 16:18:41 -0300

    Estrutura dos endpoints de Alterar, Listar e Deletar no ClientesController

---
+++ Backend/CadastroClientes/Models/Repository/ClientesRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace CadastroClientes.Models.Repository
+{
+    public class ClientesRepository
+    {
+        public List<Clientes> Listar()
+        {
+            List<Clientes> clientesLista = new List<Clientes>(); 
+
+            Clientes cliente = new Clientes();
+            cliente.UF = "SP";
+            cliente.Fax = "123456789";
+            cliente.Telefone = "987654321";
+            cliente.Documento = "123456789";
+            cliente.Email = "amanda@gmail.com";
+            cliente.Nome = "Amanda";
+            cliente.Sexo = "Feminino";
+
+            return null;
+        }
+    }
+}
